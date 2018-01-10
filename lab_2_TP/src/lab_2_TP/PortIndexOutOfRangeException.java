7cd5e26fb82828e7717341aeca9adf19dcfb176a
ArinaYurtanova <31742022+ArinaYurtanova@users.noreply.github.com>
2018-01-10 20:47:14 +0400

    Add files via upload

---
+++ lab_2_TP/src/lab_2_TP/PortIndexOutOfRangeException.java
@@ -0,0 +1,8 @@
+package lab_2_TP;
+
+public class PortIndexOutOfRangeException extends Exception {
+	public PortIndexOutOfRangeException() {
+		super("Нет корабля с указанном месте");
+	}
+
+}
---
+++ lab_2_TP/src/lab_2_TP/PortOverflowException.java
@@ -0,0 +1,9 @@
+package lab_2_TP;
+
+public class PortOverflowException extends Exception {
+	 	 
+	 	 	public PortOverflowException() {
+	 	 		super("Порт переполнен");
+	 	 	}
+}
+
